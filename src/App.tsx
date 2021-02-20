fa492de366991a60b62666fc46977f1c3ef5ef41
Neldin Džeković <neldin.dzeko@gmail.com>
2021-02-21 00:49:39 +0100

    Add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import { Container, Row, Col } from 'react-bootstrap';
 import { JobsPage } from './pages/JobsPage';
+import { CompaniesPage } from './pages/CompaniesPage';
 
 function App() {
   return (
     <div className="bg-light" style={{ width: '100%', height: '100%' }}>
-      <Container>
-        <Row>
-          <Col sm="12">
-            <JobsPage />
-          </Col>
-        </Row>
-      </Container>
+      <Router>
+        <Container>
+          <Row>
+            <Col sm="12">
+              <Switch>
+                <Route path="/jobs" component={JobsPage} />
+                <Route path="/companies" component={CompaniesPage} />
+              </Switch>  
+            </Col>
+          </Row>
+          </Container>
+      </Router>
     </div>
   );
 }
