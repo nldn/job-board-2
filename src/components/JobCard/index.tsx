53b3a3846a1f41a63cc0a74e03abd08e0006e484
Neldin Džeković <neldin.dzeko@gmail.com>
2021-02-21 00:01:58 +0100

    Add JobCard component

---
+++ src/components/JobCard/index.tsx
@@ -0,0 +1,25 @@
+import { Row, Col, Card, Button } from 'react-bootstrap';
+
+import { ReactComponent as CompanyLogo } from '../CompanyCard/apple.svg';
+
+export const JobCard = () => {
+  return (
+    <Card style={{ marginBottom: 15 }}>
+      <Card.Body>
+        <Row>
+          <Col sm="1" style={{ display: 'flex' }}>
+            <CompanyLogo />
+          </Col>
+          <Col sm="11">
+            <Card.Title>Frontend Developer</Card.Title>
+            <Card.Subtitle className="text-muted">Apple Inc.</Card.Subtitle>
+            <Card.Text>
+              We are looking for a frontend developer who will join our Design & Engineering team.
+            </Card.Text>
+            <Button variant="outline-dark">More</Button>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+};
