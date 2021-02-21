0426b911c2d801657c202496cf7dcdd69cd1b116
Neldin Džeković <neldin.dzeko@gmail.com>
2021-02-21 17:58:38 +0100

    Add jobs list to company details page

--- src/components/JobCard/index.tsx
+++ src/components/JobCard/index.tsx
@@ -10,16 +10,20 @@ export const JobCard = ({ job }) => {
     <Card style={{ marginBottom: 15 }}>
       <Card.Body>
         <Row>
-          <Col sm="1" style={{ display: 'flex' }}>
-            <CompanyLogo name={job.company.logo} />
-          </Col>
+          {job.company && (
+            <Col sm="1" style={{ display: 'flex' }}>
+              <CompanyLogo name={job.company.logo} />
+            </Col>
+          )}
           <Col sm="11">
             <Card.Title>
               {job.title}
             </Card.Title>
-            <Card.Subtitle className="text-muted">
-              {job.company !== null ? job.company.name : 'Unknown Company'}
-            </Card.Subtitle>
+            {job.company && (
+              <Card.Subtitle className="text-muted">
+                {job.company.name}
+              </Card.Subtitle>
+            )}
             <Card.Text>
               {job.description}
             </Card.Text>
